85c3051908def64a9568507146bc148b026e3459
Priya Kourav <priyakourav55@gmail.com>
2026-02-03 22:03:33 +0530

    update  Contains_duplicate.cpp
    Removed comments explaining the problem statement.

--- Array/Contains_duplicate.cpp
+++ Array/Contains_duplicate.cpp
@@ -1,9 +1,6 @@
 
 // QNO.217  Contains Duplicate
 
-
-    // Given an integer array nums, return true if any value appears at least twice in the array,
-    // and return false if every element is distinct.
 #include <iostream>
 #include <vector>
 #include <unordered_set>
