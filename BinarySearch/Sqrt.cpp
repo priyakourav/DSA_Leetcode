bcee883c0a3febc7119a172700381a4d97baca61
Priya Kourav <priyakourav55@gmail.com>
2026-02-08 18:08:48 +0530

    add sqrt.cpp file

---
+++ BinarySearch/Sqrt.cpp
@@ -0,0 +1,35 @@
+//QNO. 69 SQUARE ROOT
+
+#include <bits/stdc++.h>
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==0) return 0;
+        int left=1;
+        int right=x;
+        int ans=0;
+        while(left<=right)
+        {
+            int mid=left+(right-left)/2;
+            if(mid<=x/mid)
+            {
+                ans=mid;
+                left=mid+1;
+            }
+            else
+            {
+                right=mid-1;
+            }
+        }
+        return ans;
+    }
+};
+
+    int main()
+    {
+        int x=16;
+        Solution obj;
+        int result=obj.mySqrt(x);
+        std::cout<<result;
+        return 0;
+    }
