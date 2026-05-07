1063bffb7efb8eaf1dad36c0281f33b61c7b426c
Priya Kourav <priyakourav55@gmail.com>
2026-05-07 19:35:30 +0530

    added fizz buzz file

---
+++ String/fizz_buzz.cpp
@@ -0,0 +1,32 @@
+//QNO.412 FIZZ BUZZ
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+vector<string> fizzBuzz(int n) {
+    vector<string> result;
+    for (int i = 1; i <= n; ++i) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            result.push_back("FizzBuzz");
+        } else if (i % 3 == 0) {
+            result.push_back("Fizz");
+        } else if (i % 5 == 0) {
+            result.push_back("Buzz");
+        } else {
+            result.push_back(to_string(i));
+        }
+    }
+    return result;
+}
+
+int main() {
+    int n = 15; 
+    vector<string> output = fizzBuzz(n);
+    
+    for (const string& str : output) {
+        cout << str << endl;
+    }
+    
+    return 0;
+}
